d698ea2ee73df7219473afbddb9af2449893fcf5
attackedserver <166152010+attackedserver@users.noreply.github.com>
2024-04-06 01:12:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,337 +1,276 @@
-var width, height, container, canvas, ctx, points, target, animateHeader = true;
+var canvas = document.querySelector(".hacker-3d-shiz"),
+    ctx = canvas.getContext("2d"),
+    canvasBars = document.querySelector(".bars-and-stuff"),
+    ctxBars = canvasBars.getContext("2d"),
+    outputConsole = document.querySelector(".output-console");
 
-function init() {
-  initHeader();
-  initAnimation();
-  addListeners();
+canvas.width = (window.innerWidth/3)*2;
+canvas.height = window.innerHeight / 3;
+
+canvasBars.width = window.innerWidth/3;
+canvasBars.height = canvas.height;
+
+outputConsole.style.height = (window.innerHeight / 3) * 2 + 'px';
+outputConsole.style.top = window.innerHeight / 3 + 'px'
+
+
+/* Graphics stuff */
+function Square(z) {
+    this.width = canvas.width/2;
+    
+    if(canvas.height < 200){
+      this.width = 200;
+    };
+  
+    this.height = canvas.height;
+    z = z || 0;
+
+    this.points = [
+    new Point({
+        x: (canvas.width / 2) - this.width,
+        y: (canvas.height / 2) - this.height,
+        z: z
+    }),
+    new Point({
+        x: (canvas.width / 2) + this.width,
+        y: (canvas.height / 2) - this.height,
+        z: z
+    }),
+    new Point({
+        x: (canvas.width / 2) + this.width,
+        y: (canvas.height / 2) + this.height,
+        z: z
+    }),
+    new Point({
+        x: (canvas.width / 2) - this.width,
+        y: (canvas.height / 2) + this.height,
+        z: z
+    })];
+    this.dist = 0;
 }
 
-function initHeader() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  target = {
-    x: width / 2,
-    y: height / 2
-  };
-
-  container = document.getElementById('connecting-dots');
-  container.style.height = height + 'px';
-
-  canvas = document.getElementById('canvas');
-  canvas.width = width;
-  canvas.height = height;
-  ctx = canvas.getContext('2d');
-
-  // create points
-  points = [];
-  for (var x = 0; x < width; x = x + width / 20) {
-    for (var y = 0; y < height; y = y + height / 20) {
-      var px = x + Math.random() * width / 100;
-      var py = y + Math.random() * height / 100;
-      var p = {
-        x: px,
-        originX: px,
-        y: py,
-        originY: py
-      };
-      points.push(p);
-    }
-  }
-
-  // for each point find the 5 closest points
-  for (var i = 0; i < points.length; i++) {
-    var closest = [];
-    var p1 = points[i];
-    for (var j = 0; j < points.length; j++) {
-      var p2 = points[j]
-      if (!(p1 == p2)) {
-        var placed = false;
-        for (var k = 0; k < 5; k++) {
-          if (!placed) {
-            if (closest[k] == undefined) {
-              closest[k] = p2;
-              placed = true;
-            }
-          }
+Square.prototype.update = function () {
+    for (var p = 0; p < this.points.length; p++) {
+        this.points[p].rotateZ(0.001);
+        this.points[p].z -= 3;
+        if (this.points[p].z < -300) {
+            this.points[p].z = 2700;
         }
-
-        for (var k = 0; k < 5; k++) {
-          if (!placed) {
-            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
-              closest[k] = p2;
-              placed = true;
-            }
-          }
-        }
-      }
+        this.points[p].map2D();
     }
-    p1.closest = closest;
-  }
-
-  // assign a circle to each point
-  for (var i in points) {
-    var c = new Circle(points[i], 2 + Math.random() * 2, 'rgba(255,255,255,0.9)');
-    points[i].circle = c;
-  }
 }
 
-// Event handling
-function addListeners() {
-  if (!('ontouchstart' in window)) {
-  //  window.addEventListener("mousemove", mouseMove);
-  }
-  window.addEventListener("resize", resize, true);
-  window.addEventListener("scroll", scrollCheck);
-}
-
-function mouseMove(e) {
-  var posx = posy = 0;
-  if (e.pageX || e.pageY) {
-    posx = e.pageX;
-    posy = e.pageY;
-  } else if (e.clientX || e.clientY) {
-    posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-    posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-  }
-  target.x = posx;
-  target.y = posy;
-}
-
-function scrollCheck() {
-  if (document.body.scrollTop > height) animateHeader = false;
-  else animateHeader = true;
-}
-
-function resize() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  container.style.height = height + 'px';
-  ctx.canvas.width = width;
-  ctx.canvas.height = height;
-}
-
-// animation
-function initAnimation() {
-  animate();
-  for (var i in points) {
-    shiftPoint(points[i]);
-  }
-}
-
-function animate() {
-  if (animateHeader) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i in points) {
-      // detect points in range
-      if (Math.abs(getDistance(target, points[i])) < 4000) {
-        points[i].active = 0.3;
-        points[i].circle.active = 0.6;
-      } else if (Math.abs(getDistance(target, points[i])) < 20000) {
-        points[i].active = 0.1;
-        points[i].circle.active = 0.3;
-      } else if (Math.abs(getDistance(target, points[i])) < 40000) {
-        points[i].active = 0.02;
-        points[i].circle.active = 0.1;
-      } else {
-        points[i].active = 0;
-        points[i].circle.active = 0;
-      }
-
-      drawLines(points[i]);
-      points[i].circle.draw();
-    }
-  }
-  requestAnimationFrame(animate);
-}
-
-function shiftPoint(p) {
-  TweenLite.to(p, 1 + 1 * Math.random(), {
-    x: p.originX - 50 + Math.random() * 100,
-    y: p.originY - 50 + Math.random() * 100,
-    ease: Circ.easeInOut,
-    onComplete: function() {
-      shiftPoint(p);
-    }
-  });
-}
-
-// Canvas manipulation
-function drawLines(p) {
-  if (!p.active) return;
-  for (var i in p.closest) {
+Square.prototype.render = function () {
     ctx.beginPath();
-    ctx.moveTo(p.x, p.y);
-    ctx.lineTo(p.closest[i].x, p.closest[i].y);
-    ctx.strokeStyle = 'rgba(255,255,255,' + p.active + ')';
+    ctx.moveTo(this.points[0].xPos, this.points[0].yPos);
+    for (var p = 1; p < this.points.length; p++) {
+        if (this.points[p].z > -(focal - 50)) {
+            ctx.lineTo(this.points[p].xPos, this.points[p].yPos);
+        }
+    }
+
+    ctx.closePath();
     ctx.stroke();
-  }
+
+    this.dist = this.points[this.points.length - 1].z;
+
+};
+
+function Point(pos) {
+    this.x = pos.x - canvas.width / 2 || 0;
+    this.y = pos.y - canvas.height / 2 || 0;
+    this.z = pos.z || 0;
+
+    this.cX = 0;
+    this.cY = 0;
+    this.cZ = 0;
+
+    this.xPos = 0;
+    this.yPos = 0;
+    this.map2D();
 }
 
-function Circle(pos, rad, color) {
-  var _this = this;
+Point.prototype.rotateZ = function (angleZ) {
+    var cosZ = Math.cos(angleZ),
+        sinZ = Math.sin(angleZ),
+        x1 = this.x * cosZ - this.y * sinZ,
+        y1 = this.y * cosZ + this.x * sinZ;
 
-  // constructor
-  (function() {
-    _this.pos = pos || null;
-    _this.radius = rad || null;
-    _this.color = color || null;
-  })();
-
-  this.draw = function() {
-    if (!_this.active) return;
-    ctx.beginPath();
-    ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'rgba(255,255,255,' + _this.active + ')';
-    ctx.fill();
-  };
+    this.x = x1;
+    this.y = y1;
 }
 
-// Util
-function getDistance(p1, p2) {
-  return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+Point.prototype.map2D = function () {
+    var scaleX = focal / (focal + this.z + this.cZ),
+        scaleY = focal / (focal + this.z + this.cZ);
+
+    this.xPos = vpx + (this.cX + this.x) * scaleX;
+    this.yPos = vpy + (this.cY + this.y) * scaleY;
+};
+
+// Init graphics stuff
+var squares = [],
+    focal = canvas.width / 2,
+    vpx = canvas.width / 2,
+    vpy = canvas.height / 2,
+    barVals = [],
+    sineVal = 0;
+
+/* fake console stuff */
+var commandStart = ['Performing DNS Lookups for', 
+                'Searching ', 
+                'Analyzing ', 
+                'Estimating Approximate Location of ', 
+                'Compressing ', 
+                'Requesting Authorization From : ', 
+                'wget -a -t ', 
+                'tar -xzf ', 
+                'Entering Location ', 
+                'Compilation Started of ',
+                 'Downloading '],
+    commandParts = ['Data Structure', 
+                    'http://wwjd.com?au&2', 
+                    'Texture', 
+                    'TPS Reports', 
+                    ' .... Searching ... ', 
+                    'http://zanb.se/?23&88&far=2', 
+                    'http://ab.ret45-33/?timing=1ww'],
+    commandResponses = ['Authorizing ', 
+                 'Authorized...', 
+                 'Access Granted..', 
+                 'Going Deeper....', 
+                 'Compression Complete.', 
+                 'Compilation of Data Structures Complete..', 
+                 'Entering Security Console...', 
+                 'Encryption Unsuccesful Attempting Retry...', 
+                 'Waiting for response...', 
+                 '....Searching...', 
+                 'Calculating Space Requirements '
+                ],
+    isProcessing = false,
+    processTime = 0,
+    lastProcess = 0;
+
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    squares.sort(function (a, b) {
+        return b.dist - a.dist;
+    });
+    for (var i = 0, len = squares.length; i < len; i++) {
+        squares[i].update();
+        squares[i].render();
+    }
+    
+    ctxBars.clearRect(0, 0, canvasBars.width, canvasBars.height);
+    
+    ctxBars.beginPath();
+    var y = canvasBars.height/6;
+    ctxBars.moveTo(0,y);
+    
+    for(i = 0; i < canvasBars.width; i++){
+        var ran = (Math.random()*20)-10;
+        if(Math.random() > 0.98){
+             ran = (Math.random()*50)-25   
+        }
+        ctxBars.lineTo(i, y + ran);
+    }
+    
+    ctxBars.stroke();
+    
+    for(i = 0; i < canvasBars.width; i+=20){
+        if(!barVals[i]){
+            barVals[i] = {
+                val : Math.random()*(canvasBars.height/2),
+                freq : 0.1,
+                sineVal : Math.random()*100
+            };
+        }
+        
+        barVals[i].sineVal+=barVals[i].freq;
+        barVals[i].val+=Math.sin(barVals[i].sineVal*Math.PI/2)*5;
+        ctxBars.fillRect(i+5,canvasBars.height,15,-barVals[i].val);
+    }
+    
+    requestAnimationFrame(render);
 }
 
-init();
+function consoleOutput(){
+    var textEl = document.createElement('p');
+    
+    if(isProcessing){
+        textEl = document.createElement('span');
+        textEl.textContent += Math.random() + " ";
+        if(Date.now() > lastProcess + processTime){
+            isProcessing = false;   
+        }
+    }else{
+        var commandType = ~~(Math.random()*4);
+        switch(commandType){
+            case 0:
+                textEl.textContent = commandStart[~~(Math.random()*commandStart.length)] + commandParts[~~(Math.random()*commandParts.length)];
+                break;
+            case 3: 
+                isProcessing = true;
+                processTime = ~~(Math.random()*5000);
+                lastProcess = Date.now();
+            default:
+                 textEl.textContent = commandResponses[~~(Math.random()*commandResponses.length)];
+            break;
+        }
+    }
 
-
-;(function(window) {
-
-	'use strict';
-
-		//FIND IP
-
-		function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
-		  var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
-		  var pc = new myPeerConnection({iceServers: []}),
-		    noop = function() {},
-		    localIPs = {},
-		    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-		    key;
-
-		  function ipIterate(ip) {
-		    if (!localIPs[ip]) onNewIP(ip);
-		    localIPs[ip] = true;
-		  }
-		  pc.createDataChannel(""); //create a bogus data channel
-		  pc.createOffer(function(sdp) {
-		    sdp.sdp.split('\n').forEach(function(line) {
-		      if (line.indexOf('candidate') < 0) return;
-		      line.match(ipRegex).forEach(ipIterate);
-		    });
-		    pc.setLocalDescription(sdp, noop, noop);
-		  }, noop); // create offer and set local description
-		  pc.onicecandidate = function(ice) { //listen for candidate events
-		    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-		    ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
-		  };
-		}
-
-function addIP(ip) {
-  console.log('got ip: ', ip);
-
-	var theIp = document.getElementById('ip');
-	var theConsole = $('span.console');
-	var texted = ip;
-
-  theIp.textContent = ip;
-
-
-
-	theConsole.html(texted);
-
-}
-
-findIP(addIP);
-
-//FIND LOCATIOn
-
-
-$.getJSON('https://ipapi.co/'+$(ip).val()+'/json', function(data){
-
-      $('.country').text(data.country);
-  });
-
-
-	(function() {
-
-		var theConsole = $('span.console');
-		var texted = $("#ip").text();
-
-		theConsole.html(texted);
-	});
-
-var search_form = document.getElementsByClassName('search__form');
-console.log(search_form);
-
-
-
-function createHome(){
-
-  var homeDiv = document.createElement('div');
-        homeDiv.innerHTML = '<div class="home_container"><h2>I am hungry</h2><p>Shall we go eat?</p><div class="close_home" href="">x</div></div>';
-        homeDiv.setAttribute('class', 'home');
-        document.body.appendChild(homeDiv);
-
-        $('.close_home').click(function(){
-            $('.home').remove();
-            console.log('Home Erased');
-        });
-
-
+    outputConsole.scrollTop = outputConsole.scrollHeight;
+    outputConsole.appendChild(textEl);
+    
+    if (outputConsole.scrollHeight > window.innerHeight) {
+       var removeNodes = outputConsole.querySelectorAll('*');
+       for(var n = 0; n < ~~(removeNodes.length/3); n++){
+            outputConsole.removeChild(removeNodes[n]);
+        }
+    }
+    
+    setTimeout(consoleOutput, ~~(Math.random()*200));
 }
 
 
-var navigationLink = $('.terminal__line a');
+setTimeout(function(){   
+      canvas.width = (window.innerWidth/3)*2;
+      canvas.height = window.innerHeight / 3;
 
-navigationLink.click(function(e){
-  if ($(this).hasClass('out')) {
-    window.open('https://t.me/attackedserver');
-  }else
-  {
-  createHome();
-  }
-});
+      canvasBars.width = window.innerWidth/3;
+      canvasBars.height = canvas.height;
 
+      outputConsole.style.height = (window.innerHeight / 3) * 2 + 'px';
+      outputConsole.style.top = window.innerHeight / 3 + 'px';
+  
+      focal = canvas.width / 2;
+      vpx = canvas.width / 2;
+      vpy = canvas.height / 2;
 
-
-	$(search_form).submit(function( event ) {
-	  if ( 'aboutme' === $( "input" ).val() || 'codelab' === $( "input" ).val() ||  'contact' === $( "input" ).val() || 'gethacked' === $( "input" ).val() || 'blog' === $( "input" ).val() || 'home'  === $( "input" ).val()) {
-
-    createHome();
-
-	  } else if ( $( "input" ).val() === "instagram" ) {
-				window.open('http://instagram.com/arcticben.co.uk');
-  		} else if ($( "input" ).val() === "ipconfig") {
-
-        var binder = $('input').val();
-        var terminal_div = document.getElementsByClassName('terminal');
-            $('.terminal').addClass("binding");
-        var theipagain = $('#ip').html();
-
-        var ipconfig = document.createElement('p');
-              $(ipconfig).text('ipconfig: ' + theipagain);
-              ipconfig.setAttribute('class', 'terminal__line');
-              $(ipconfig).appendTo(terminal_div);
-              console.log(ipconfig.length);
-
+      for (var i = 0; i < 15; i++) {
+          squares.push(new Square(-300 + (i * 200)));
       }
+  
+      ctx.strokeStyle = ctxBars.strokeStyle = ctxBars.fillStyle = '#00FF00';
+  
+      render();
+      consoleOutput();
+}, 200);
 
-		var binder = $('input').val();
-		var terminal_div = document.getElementsByClassName('terminal');
-				$('.terminal').addClass("binding");
+window.addEventListener('resize', function(){
+      canvas.width = (window.innerWidth/3)*2;
+      canvas.height = window.innerHeight / 3;
 
-		var commands = document.createElement('p');
-					commands.innerHTML = ('Execute: ' + binder);
-					commands.setAttribute('class', 'terminal__line');
-					$(commands).appendTo(terminal_div);
+      canvasBars.width = window.innerWidth/3;
+      canvasBars.height = canvas.height;
 
-
-
-
-
-	  event.preventDefault();
+      outputConsole.style.height = (window.innerHeight / 3) * 2 + 'px';
+      outputConsole.style.top = window.innerHeight / 3 + 'px';
+  
+      focal = canvas.width / 2;
+      vpx = canvas.width / 2;
+      vpy = canvas.height / 2;
+      ctx.strokeStyle = ctxBars.strokeStyle = ctxBars.fillStyle = '#00FF00';
 });
-
-
-
-})(window);
