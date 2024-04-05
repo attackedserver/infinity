85489aadaeef686db165d04fcb1581774a6f2fa0
attackedserver <166152010+attackedserver@users.noreply.github.com>
2024-04-05 20:58:40 +0300

    script.js

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
+var width, height, container, canvas, ctx, points, target, animateHeader = true;
 
-  if ($(this).hasClass('out')) {
-    window.open('https://t.me/attackedserver');
-  }else
-  {
-  createHome();
-  }
-});
+function init() {
+  initHeader();
+  initAnimation();
+  addListeners();
+}
 
+function initHeader() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  target = {
+    x: width / 2,
+    y: height / 2
+  };
 
+  container = document.getElementById('connecting-dots');
+  container.style.height = height + 'px';
 
-	$(search_form).submit(function( event ) {
-	  if ( 'aboutme' === $( "input" ).val() || 'codelab' === $( "input" ).val() ||  'contact' === $( "input" ).val() || 'gethacked' === $( "input" ).val() || 'blog' === $( "input" ).val() || 'home'  === $( "input" ).val()) {
+  canvas = document.getElementById('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  ctx = canvas.getContext('2d');
 
-    createHome();
-
-	  } else if ( $( "input" ).val() === "instagram" ) {
-				window.open('http://instagram.com/arcticben.co.uk');
-  		} else if ($( "input" ).val() === "ipconfig") {
-
-        var binder = $('input').val();
-        var terminal_div = document.getElementsByClassName('terminal');
-            $('.terminal').addClass("binding");
-        var theipagain = $('#ip').html();
-
-        var ipconfig = document.createElement('p');
-              $(ipconfig).text('ipconfig: ' + theipagain);
-              ipconfig.setAttribute('class', 'terminal__line');
-              $(ipconfig).appendTo(terminal_div);
-              console.log(ipconfig.length);
-
-      }
-
-		var binder = $('input').val();
-		var terminal_div = document.getElementsByClassName('terminal');
-				$('.terminal').addClass("binding");
-
-		var commands = document.createElement('p');
-					commands.innerHTML = ('Execute: ' + binder);
-					commands.setAttribute('class', 'terminal__line');
-					$(commands).appendTo(terminal_div);
-
-
-
+  // create points
+  points = [];
+  for (var x = 0; x < width; x = x + width / 20) {
+    for (var y = 0; y < height; y = y + height / 20) {
+      var px = x + Math.random() * width / 100;
+      var py = y + Math.random() * height / 100;
+      var p = {
+        x: px,
